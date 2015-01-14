7a5df2c99a15a90207f6daca5f00df135fe82a7d
Joseph Wynn <joseph@wildlyinaccurate.com>
2015-01-14 21:01:53 +0000

    Implement DecimalToNumeral#generate

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function DecimalToNumeral() {}
 
 DecimalToNumeral.prototype.MAX_DECIMAL_VALUE = 3999;
-
 DecimalToNumeral.prototype.mapping = {
     1: 'I',
     4: 'IV',
@@ -10,20 +9,57 @@ DecimalToNumeral.prototype.mapping = {
     10: 'X',
     40: 'XL',
     50: 'L',
-    90: 'LC',
+    90: 'XC',
     100: 'C',
     400: 'CD',
     500: 'D',
-    900: 'DM',
+    900: 'CM',
     1000: 'M',
 };
 
-DecimalToNumeral.prototype.convert = function(decimal) {
+/**
+ * Convert an integer to roman numeral
+ *
+ * @param  {number} decimal
+ * @return {string}
+ */
+DecimalToNumeral.prototype.generate = function(decimal) {
     if (decimal > this.MAX_DECIMAL_VALUE) {
         throw new Error('Unable to convert numbers larger than ' + this.MAX_DECIMAL_VALUE);
     }
 
-    return this.mapping[decimal];
+    if (decimal < 0) {
+        throw new Error('Unable to generate roman numerals below zero.');
+    }
+
+    if (decimal === 0) {
+        // The only edge case; 'nulla' is written to represent zero (literally
+        // 'none' in latin)
+        return 'nulla';
+    }
+
+    var mapping = this.mapping;
+    var numeralValues = Object.keys(mapping).sort(sortNumeric).reverse();
+    var numeral = '';
+
+    numeralValues.forEach(function(value) {
+        while (decimal >= value) {
+            decimal = decimal - value;
+            numeral = numeral + mapping[value];
+        }
+    });
+
+    return numeral;
 };
+
+/**
+ * Compare function for Array.prototype.sort, for sorting numerically
+ * @param  {*} a
+ * @param  {*} b
+ * @return {boolean}
+ */
+function sortNumeric(a, b) {
+    return a - b;
+}
 
 module.exports = new DecimalToNumeral();
--- test.js
+++ test.js
@@ -1,8 +1,14 @@
 var assert = require('assert');
 var decimalToNumeral = require('./index');
 
-assert.equal('I', decimalToNumeral.convert(1));
-assert.equal('II', decimalToNumeral.convert(2));
-assert.equal('XIII', decimalToNumeral.convert(13));
-assert.equal('MMMCMXCIX', decimalToNumeral.convert(3999));
-assert.throws(decimalToNumeral.convert.bind(decimalToNumeral, 4000), Error);
+assert.equal('I', decimalToNumeral.generate(1));
+assert.equal('II', decimalToNumeral.generate(2));
+assert.equal('XIII', decimalToNumeral.generate(13));
+assert.equal('XXXVIII', decimalToNumeral.generate(38));
+assert.equal('LXVII', decimalToNumeral.generate(67));
+assert.equal('DCCVII', decimalToNumeral.generate(707));
+assert.equal('MMMCMXCIX', decimalToNumeral.generate(3999));
+assert.throws(decimalToNumeral.generate.bind(decimalToNumeral, 4000), Error);
+assert.throws(decimalToNumeral.generate.bind(decimalToNumeral, -1), Error);
+
+console.log('All tests passed!');
